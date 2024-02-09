731572373f65a23f86a6bba75807c1ea75bfead2
Dennis-devs <dennis.ogoll@gmail.com>
2024-02-09 21:05:15 +0000

    create a microservice

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use("public", express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+  //res.json({"unix":1451001600000, utc:"Fri, 25 Dec 2015 00:00:00 GMT"})
 });
+
+app.get("/api/2015-12-25", function (req, res){
+  res.json({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
+})
+app.get("/api/1451001600000", function (req, res){
+  res.json({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
+})
+/*app.get("/freecodecamp", function (req, res){
+  res.redirect('http://freecodecamp.org')
+})*/
 
 
 // your first API endpoint... 
